5387c0fd1f2027e545edcbf3a70468ed330f1ea8
Yogesh <151730733+Yogesh759@users.noreply.github.com>
2024-07-09 16:58:49 +0000

    conversion

---
+++ 1_/3_conversionoperation.js
@@ -0,0 +1,8 @@
+const itNumber= true
+let mtNumber=Boolean(itNumber)
+console.log(typeof mtNumber);
+
+let score="45abc"
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber)
