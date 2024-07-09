816d3e48c0990bf5be5a977dd3b8c83e291e572a
Yogesh <151730733+Yogesh759@users.noreply.github.com>
2024-07-09 16:26:21 +0000

    data type

---
+++ 1_/1_variable.js
@@ -0,0 +1,10 @@
+const accountId=14454
+let accountEmail ="yogesh@google";
+let accountPass="123455"
+let accountState="harayana";
+accountcity="har";
+// console.log(accountEmail);
+
+accountEmail ="yogesh@goo";
+accountPass="125"
+console.table([accountId, accountEmail, accountPass, accountState, accountcity]);
---
+++ 1_/2_datatype.js
@@ -0,0 +1,10 @@
+"use strict";//treat all code as a newer version
+
+// alert(3+3)//we are using node.js not browser
+ 
+
+let name="yogesh"
+let age = 19
+let isboolean = false
+
+console.log(typeof isboolean);
